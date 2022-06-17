8514218a0f9b701a108ebb95b9c920c576c5e4b7
lace <ronaldqinbiao@163.com>
2022-06-17 16:26:31 +0800

    update

---
+++ src/main/java/com/example/bean/User.java
@@ -0,0 +1,11 @@
+package com.example.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private String name;
+    private String password;
+}
