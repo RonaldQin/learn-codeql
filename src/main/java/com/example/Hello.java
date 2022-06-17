5f8b92f0696dfe89e0e616f8d80fccb1df021dcd
lace <ronaldqinbiao@163.com>
2022-06-17 16:52:02 +0800

    rewrite lombok.

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -2,13 +2,13 @@ package com.example;
 
 import com.example.bean.User;
 
-import lombok.extern.slf4j.Slf4j;
+// import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+// @Slf4j
 public class Hello {
     public static void main(String[] args) {
         User user = new User("lace", "admin");
-        log.info("User: " + user.toString() + " learns CodeQL!");
-        log.info("Hello CodeQL!");
+        System.out.println("User: " + user.toString() + " learns CodeQL!");
+        System.out.println("Hello CodeQL!");
     }
 }
--- src/main/java/com/example/bean/User.java
+++ src/main/java/com/example/bean/User.java
@@ -1,11 +1,21 @@
 package com.example.bean;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
 
-@Data
-@AllArgsConstructor
+// @Data
+// @AllArgsConstructor
 public class User {
     private String name;
     private String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
