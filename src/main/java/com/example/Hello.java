b6d62d12191d3378669ee5da03219aebc7b3a78c
lace <ronaldqinbiao@163.com>
2022-06-17 16:27:01 +0800

    reupdate

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -1,10 +1,14 @@
 package com.example;
 
+import com.example.bean.User;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Hello {
     public static void main(String[] args) {
+        User user = new User("lace", "admin");
+        log.info("User: " + user.toString() + " learns CodeQL!");
         log.info("Hello CodeQL!");
     }
 }
